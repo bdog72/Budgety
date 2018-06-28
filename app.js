a9dd9da0ef3c48f6a0f9a6b91f40628b1a43e29c
Brian Bycynski <high5bri@hotmail.com>
2018-06-28 05:55:47 -0400

    finished lecture 80

---
+++ app.js
@@ -0,0 +1,63 @@
+// Budget Controller
+
+var budgetController = (function() {
+  // Some Code
+})();
+
+// UI Controller
+
+var UIController = (function() {
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  };
+
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // Will be either Inc or Exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
+})();
+
+// Global App Controller
+
+var controller = (function(budgetCtrl, UICtrl) {
+  var setupEventListeners = function() {
+    var DOM = UICtrl.getDOMstrings();
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+      if (event.keyCode === 13 || event.keyCode === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+  var ctrlAddItem = function() {
+    // 1. Get the field Input Data
+    var input = UICtrl.getInput();
+
+    // 2. Add the item to the budget controller
+    // 3. Add the item to the UI
+    // 4. Calculate the budget
+    // 5. Display the budget on the UI
+  };
+  return {
+    init: function() {
+      console.log('Application has started');
+      setupEventListeners();
+    }
+  };
+})(budgetController, UIController);
+
+controller.init();
